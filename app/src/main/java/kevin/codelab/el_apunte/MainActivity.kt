d0d75e27afb4ad7d5b12ec629162eaac9312dc57
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-22 18:37:31 +0000

    setting up databinding

--- app/src/main/java/kevin/codelab/el_apunte/MainActivity.kt
+++ app/src/main/java/kevin/codelab/el_apunte/MainActivity.kt
@@ -2,10 +2,15 @@ package kevin.codelab.el_apunte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kevin.codelab.el_apunte.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
