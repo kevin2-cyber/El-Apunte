816f3fff834d5db666c66c115846c52393c7ddf4
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-28 11:40:24 +0000

    added firebase to the app

--- app/src/main/java/kevin/codelab/el_apunte/MainActivity.kt
+++ app/src/main/java/kevin/codelab/el_apunte/MainActivity.kt
@@ -2,15 +2,20 @@ package kevin.codelab.el_apunte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kevin.codelab.el_apunte.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // init firebase
+        var auth = FirebaseAuth.getInstance()
     }
 }
