dc7f92bae57140ae8898423db61a4772620d53db
kevin2-cyber <keduful@st.ug.edu.gh>
2022-06-28 19:41:44 +0000

    new

--- app/src/main/java/kevin/codelab/el_apunte/view/LoginActivity.kt
+++ app/src/main/java/kevin/codelab/el_apunte/view/LoginActivity.kt
@@ -1,17 +1,28 @@
 package kevin.codelab.el_apunte.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kevin.codelab.el_apunte.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     // enable viewBinding
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        // init firebase
+        var auth = FirebaseAuth.getInstance()
+
+
+        binding.tvSignUp.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
--- app/src/main/java/kevin/codelab/el_apunte/view/SignUpActivity.kt
+++ app/src/main/java/kevin/codelab/el_apunte/view/SignUpActivity.kt
@@ -1,17 +1,27 @@
 package kevin.codelab.el_apunte.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kevin.codelab.el_apunte.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
 
     // enable viewBinding
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        var auth = FirebaseAuth.getInstance()
+
+
+        binding.tvSignInActivity.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
     }
 }
