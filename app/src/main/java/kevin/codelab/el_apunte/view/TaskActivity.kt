075236cb89988f3f597eab46c79344b3906cb770
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-08-27 01:42:44 +0000

    worked on sign up screen

--- app/src/main/java/kevin/codelab/el_apunte/view/TaskActivity.kt
+++ app/src/main/java/kevin/codelab/el_apunte/view/TaskActivity.kt
@@ -2,15 +2,15 @@ package kevin.codelab.el_apunte.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kevin.codelab.el_apunte.databinding.ActivityHomeBinding
+import kevin.codelab.el_apunte.databinding.ActivityTaskBinding
 
 class TaskActivity : AppCompatActivity() {
 
     // enable viewBinding
-    private lateinit var binding: ActivityHomeBinding
+    private lateinit var binding: ActivityTaskBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityHomeBinding.inflate(layoutInflater)
+        binding = ActivityTaskBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
