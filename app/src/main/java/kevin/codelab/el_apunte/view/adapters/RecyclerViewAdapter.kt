52cd4cad1c8115a356744e0c5311006e6250979d
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-01 10:48:53 +0000

    setting up the adapter

--- app/src/main/java/kevin/codelab/el_apunte/view/adapters/RecyclerViewAdapter.kt
+++ app/src/main/java/kevin/codelab/el_apunte/view/adapters/RecyclerViewAdapter.kt
@@ -1,3 +1,28 @@
 package kevin.codelab.el_apunte.view.adapters
 
-class RecyclerViewAdapter {}
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import kevin.codelab.el_apunte.R
+import kevin.codelab.el_apunte.model.NoteModel
+
+class RecyclerViewAdapter(options: FirestoreRecyclerOptions<NoteModel>) :
+    FirestoreRecyclerAdapter<NoteModel, RecyclerViewAdapter.RecyclerViewHolder>(options) {
+
+
+
+    class RecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val view: RecyclerView = itemView.findViewById(R.id.recyclerView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int, model: NoteModel) {
+        TODO("Not yet implemented")
+    }
+}
