fb6e89dcb4c74c89792567a019917fc4f3fe3239
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-06 11:13:46 +0000

    added a timestamp to the model class

--- app/src/main/java/kevin/codelab/el_apunte/model/NoteModel.kt
+++ app/src/main/java/kevin/codelab/el_apunte/model/NoteModel.kt
@@ -1,7 +1,10 @@
 package kevin.codelab.el_apunte.model
 
+import com.google.firebase.Timestamp
+
 data class NoteModel(
     val title: String,
     val content: String,
-    val color: Int
+    val color: Int,
+    val timestamp: Timestamp
 )
