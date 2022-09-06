0170d4c278ae16e7834ccfc1b15aa637d5d071cb
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-06 19:28:55 +0000

    initial values for the views

--- app/src/main/java/kevin/codelab/el_apunte/view/adapters/RecyclerViewAdapter.kt
+++ app/src/main/java/kevin/codelab/el_apunte/view/adapters/RecyclerViewAdapter.kt
@@ -1,7 +1,11 @@
 package kevin.codelab.el_apunte.view.adapters
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
@@ -15,15 +19,23 @@ class RecyclerViewAdapter(options: FirestoreRecyclerOptions<NoteModel>) :
 
     class RecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
+        private var note: CardView? = null
+        private var title: TextView? = null
+        private var date: TextView? = null
+        private var colorPallet: ImageButton? = null
+        private var deleteBtn: ImageButton? = null
+
         val view: RecyclerView = itemView.findViewById(R.id.recyclerView)
+
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.card_item, parent, false)
+        return RecyclerViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int, model: NoteModel) {
-        TODO("Not yet implemented")
+
     }
 }
