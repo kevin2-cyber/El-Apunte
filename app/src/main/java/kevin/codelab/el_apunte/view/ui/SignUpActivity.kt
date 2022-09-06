69251c38cb525f5ec97765c81f8b1fc7e3956a5b
Kevin2-cyber <keduful@st.ug.edu.gh>
2022-09-06 01:04:10 +0000

    done validating data

--- app/src/main/java/kevin/codelab/el_apunte/view/ui/SignUpActivity.kt
+++ app/src/main/java/kevin/codelab/el_apunte/view/ui/SignUpActivity.kt
@@ -3,10 +3,14 @@ package kevin.codelab.el_apunte.view.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kevin.codelab.el_apunte.databinding.ActivitySignUpBinding
+import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -44,6 +48,29 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun validateData() {
+
+        // get data
+        email = binding.etEmail.text.toString().trim()
+        password = binding.etPass.text.toString().trim()
+        confPassword = binding.etPassConf.text.toString().trim()
+
+        // validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+
+            // invalid email format
+            binding.etEmail.error = "Invalid email format"
+        else if (TextUtils.isEmpty(password))
+
+            // password isn't entered
+            binding.etPass.error = "Please enter your password"
+        else if (password.length < 6)
+            binding.etPass.error = "Please enter at least 6 characters long"
+        else if (TextUtils.isEmpty(confPassword))
+            binding.etPassConf.error = "Please confirm your password"
+        else if (!Objects.equals(password, confPassword))
+            Toast.makeText(this, "Password must be the same",
+                Toast.LENGTH_LONG).show()
+        else firebaseSignUp()
 
     }
 
