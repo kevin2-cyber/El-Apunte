1563f06bb14a32e39871311b843fb0eda4e9ea86
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-09-22 00:50:30 +0000

    waste of memory

---
+++ app/src/main/java/com/kimikevin/elapunte/util/ColorUtil.java
@@ -0,0 +1,28 @@
+package com.kimikevin.elapunte.util;
+
+import com.kimikevin.elapunte.R;
+
+import java.util.Random;
+
+public class ColorUtil {
+
+    public int beige = R.color.beige;
+    public int paleBlue = R.color.pale_blue;
+    public int paleGreen = R.color.pale_green;
+    public int paleOrange = R.color.pale_orange;
+    public int paleYellow = R.color.pale_yellow;
+    public int palePink = R.color.pale_pink;
+    public int lightGrey = R.color.light_grey;
+    public int lightBlue = R.color.light_blue;
+    public int lightPink = R.color.light_pink;
+    public int mintGreen = R.color.mint_green;
+
+//    public int[] colors = {beige,paleBlue,paleGreen,
+//            paleOrange,paleYellow,palePink,
+//            lightGrey,lightBlue,lightPink,mintGreen};
+//
+//    public int getColor() {
+//        Random random = new Random();
+//        return colors[random.nextInt(colors.length)];
+//    }
+}
