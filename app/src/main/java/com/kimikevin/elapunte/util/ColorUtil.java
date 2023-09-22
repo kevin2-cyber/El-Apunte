33c41a456f591fc830e7df7883d16193c86e8cc2
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-09-22 00:54:11 +0000

    removed unnecessary variables

--- app/src/main/java/com/kimikevin/elapunte/util/ColorUtil.java
+++ app/src/main/java/com/kimikevin/elapunte/util/ColorUtil.java
@@ -6,23 +6,12 @@ import java.util.Random;
 
 public class ColorUtil {
 
-    public int beige = R.color.beige;
-    public int paleBlue = R.color.pale_blue;
-    public int paleGreen = R.color.pale_green;
-    public int paleOrange = R.color.pale_orange;
-    public int paleYellow = R.color.pale_yellow;
-    public int palePink = R.color.pale_pink;
-    public int lightGrey = R.color.light_grey;
-    public int lightBlue = R.color.light_blue;
-    public int lightPink = R.color.light_pink;
-    public int mintGreen = R.color.mint_green;
+    public int[] colors = {R.color.beige,R.color.pale_blue,R.color.pale_green,
+            R.color.pale_orange,R.color.pale_yellow,R.color.pale_pink,
+            R.color.light_grey,R.color.light_blue,R.color.light_pink,R.color.mint_green};
 
-//    public int[] colors = {beige,paleBlue,paleGreen,
-//            paleOrange,paleYellow,palePink,
-//            lightGrey,lightBlue,lightPink,mintGreen};
-//
-//    public int getColor() {
-//        Random random = new Random();
-//        return colors[random.nextInt(colors.length)];
-//    }
+    public int getColor() {
+        Random random = new Random();
+        return colors[random.nextInt(colors.length)];
+    }
 }
