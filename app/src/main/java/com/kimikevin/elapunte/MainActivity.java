8a6b5091f76f28c15a5053b2d54fcaca1b9c6831
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-10-02 00:33:35 +0000

    I still don't understand this error

--- app/src/main/java/com/kimikevin/elapunte/MainActivity.java
+++ app/src/main/java/com/kimikevin/elapunte/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.kimikevin.elapunte.databinding.ActivityMainBinding;
 import com.kimikevin.elapunte.model.entity.Note;
@@ -13,6 +16,7 @@ import com.kimikevin.elapunte.viewmodel.MainActivityViewModel;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private MainActivityViewModel viewModel;
+    private MainClickHandler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +26,19 @@ public class MainActivity extends AppCompatActivity {
         binding.setNote(new Note());
 
         viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        handler = new MainClickHandler(this);
+        binding.setClickHandler(handler);
+    }
+
+    public class MainClickHandler {
+        private Context context;
+
+        public MainClickHandler(Context context) {
+            this.context = context;
+        }
+
+        public void onButtonClick(View view) {
+            Toast.makeText(context, "Fab clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
