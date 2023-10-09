8ebcad386c4686153b1ad71be74de15af0f81c5d
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-10-09 14:35:44 +0000

    using SimpleDateFormat

--- app/src/main/java/com/kimikevin/elapunte/model/NoteDatabase.java
+++ app/src/main/java/com/kimikevin/elapunte/model/NoteDatabase.java
@@ -1,6 +1,7 @@
 package com.kimikevin.elapunte.model;
 
 import android.content.Context;
+import android.icu.text.SimpleDateFormat;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
@@ -11,6 +12,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 import com.kimikevin.elapunte.model.dao.NoteDao;
 import com.kimikevin.elapunte.model.entity.Note;
 
+import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -55,7 +57,7 @@ public abstract class NoteDatabase extends RoomDatabase {
                 Note noteOne = new Note();
                 noteOne.setTitle("Health");
                 noteOne.setContent("Hello My Friend");
-                noteOne.setDateTime("August, 7, 2023");
+                noteOne.setDateTime(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.ROOT));
 
                 noteDao.insert(noteOne);
             }
