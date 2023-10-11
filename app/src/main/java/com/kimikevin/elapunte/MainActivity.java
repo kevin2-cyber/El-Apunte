f5c5936bc366ca035d5acd6e13c2d0c04e2882cb
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-10-11 16:45:01 +0000

    using the trim method

--- app/src/main/java/com/kimikevin/elapunte/MainActivity.java
+++ app/src/main/java/com/kimikevin/elapunte/MainActivity.java
@@ -78,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                filter(s.toString());
+                filter(s.toString().trim());
             }
         });
     }
